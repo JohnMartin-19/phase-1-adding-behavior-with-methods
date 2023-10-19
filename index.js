6466237b1b417a1ce05826b1323ce63b6074d45b
John <johnnkonge2020@gmail.com>
2023-10-19 11:06:20 +0300

    keep going

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+class cat{
+    constructor(name,sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak(){
+        console.log(` ${this.name} says meow!`)
+    }
+}
+let puss = new cat('Garfiled','male')
+puss.speak()
+
+class dog{
+    constructor(name,sex){
+        this.name = name
+        this.sex = sex
+        
+    }
+    speak(){
+        console.log(` ${this.name} warns bark! bark!!`)
+    }
+}
+let doggi = new dog('mutina','male')
+doggi.speak()
+
+
+class bird{
+    constructor(name,sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak(){
+       
+        if(bird.sex = 'male'){
+            console.log(` ${this.name} says Im the man!`)
+        } else {
+            console.log(` ${this.name} says quack! quack!`)
+        }
+    }
+}
+let dush = new bird('parrot','male')
+dush.speak()
